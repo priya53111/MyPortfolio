daa9bcfdc0f79d3dc2385ceba98ab132520b4448
priya53111 <priyasolai22@gmail.com>
2024-03-20 16:43:56 +0530

    Information

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed =new Typed(".text",{
+    strings :["Frontend developer","Backend developer","Web developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+
+});
